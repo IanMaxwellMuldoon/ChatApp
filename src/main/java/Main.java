1dd9e384da3d78fe9f3e3258731ff8ea43ef4333
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2022-02-26 20:57:31 -0800

    Created Client.java

--- src/main/java/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World - Comp 429 2/19/2022");
-        System.out.println("Hello");
-    }
-}
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,26 +6,49 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server implements Runnable{
 
-    private ArrayList<ConnectionHandler> serverList;
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService pool; //thread pool
+
+    public Server(){
+        connections = new ArrayList<>();
+        done = false;
+    }
 
 
     @Override
     public void run() {
         try {
-            System.out.println("Enter a port number: ");
-            Scanner sc = new Scanner(System.in);  //Im not sure if we are supposed to use scanner to get port number
-            int portnum = sc.nextInt();
-            ServerSocket server = new ServerSocket(portnum); //Server
-            Socket client = server.accept();  //accept method returns client socket
-            ConnectionHandler handler = new ConnectionHandler(client);
-            serverList.add(handler);
-        } catch (IOException e) {  //Handle Server Shutdown
+            //System.out.println("Enter a port number: ");
+           // Scanner sc = new Scanner(System.in);  //Im not sure if we are supposed to use scanner to get port number
+           // int portnum = sc.nextInt();
+            server = new ServerSocket(9999); //Server
+            pool = Executors.newCachedThreadPool();
+            while(!done) {
+                Socket client = server.accept();  //accept method returns client socket
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }} catch (IOException e) {  //Handle Server Shutdown
             e.printStackTrace();
         }
     }
+    public void shutdown() throws IOException {
+        done = true;
+        if(!server.isClosed()){
+            server.close();
+        }
+        for(ConnectionHandler ch : connections){
+            ch.shutdown();
+        }
+    }
+
     class ConnectionHandler implements Runnable{  //Handler for each client that connects to server
         private Socket client;
         private BufferedReader in;
@@ -42,21 +65,25 @@ public class Server implements Runnable{
         public void run() {
 
             try {
+                String message = "";
                 out = new PrintWriter(client.getOutputStream(), true);
                 in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 out.println("Enter a name: ");
                 name = in.readLine();
                 System.out.println(name+ " connected!");
                 broadcast(name + " joined the chat!");
-                String message;
-                while(message = in.readLine() != null){
+                while((message = in.readLine()) != null){
                     if(message.startsWith("/name")){
                         //TODO: change nickname
                     }
                     if(message.startsWith("/quit")){
-                        //TODO: quit
+                        broadcast(name + " has left the server");
+                        shutdown();
                     }
-                    
+                    else{
+                        broadcast(name + " : " + message);
+                    }
+
 
                 }
 
@@ -70,11 +97,23 @@ public class Server implements Runnable{
             out.println(message);
         }
         public void broadcast(String message){
-            for(ConnectionHandler ch : serverList){
+            for(ConnectionHandler ch : connections){
                 if(ch != null){
                     ch.sendMessage(message);
                 }
             }
         }
+        public void shutdown() throws IOException { //client shutdown
+            in.close();
+            out.close();
+            if(!client.isClosed()){
+                client.close();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
     }
 }
