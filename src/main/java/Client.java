2f35f2d661103d8dbba179e8b692a121d605f0d9
brandonleyden <32991558+brandonleyden@users.noreply.github.com>
2022-03-02 14:09:34 -0800

    working?
    I hope this works

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class Client implements Runnable{
@@ -19,7 +20,7 @@ public class Client implements Runnable{
             out = new PrintWriter(client.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
-            InputHandler inHandler = new InputHandler();
+            InputHandler inHandler = new InputHandler(client);
             Thread t = new Thread(inHandler);
             t.start();
 
@@ -32,6 +33,10 @@ public class Client implements Runnable{
         }
     }
     class InputHandler implements Runnable{
+        private Socket client = null;
+        InputHandler(Socket client){
+            this.client = client;
+        }
 
         @Override
         public void run() {
@@ -39,11 +44,56 @@ public class Client implements Runnable{
                 BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
                 while(!done){
                     String message = inReader.readLine();
-                    if(message.equals("/quit")){
+                    if(message.equals("help")) {
+                        System.out.println ("You can select any of these commands:");
+                        System.out.println ("help");
+                        System.out.println ("myip");
+                        System.out.println ("myport");
+                        System.out.println ("Connect");
+                        System.out.println ("list");
+                        System.out.println ("terminate<connection id>");
+                        System.out.println ("send<connection id><message>");
+                        System.out.println ("exit");
+
+                    }
+                    else if(message.equals("myip")){// if not connected do nothing.
+                        //we may want to do this in server side
+                        System.out.println ("Your IP address : "+ InetAddress.getLocalHost().getHostAddress());
+                        //System.out.println ("Your IP address : "+ this.client.getInetAddress().toString());
+                    }
+                    else if(message.equals("myport")){
+                        // we may want to do this in server side
+                        System.out.println ("Your port : "+ this.client.getLocalPort());
+                    }
+                    else if(message.startsWith("connect")){
+                        String[] temp = message.split(" ");
+                        for (int i = 1; i < temp.length; i++){
+                            System.out.println ("" + temp[i]);
+                        }
+                        try {
+                            //this might work
+                            client = new Socket(InetAddress.getByName( temp[1]),Integer.parseInt(temp[2]));
+                            out = new PrintWriter(client.getOutputStream(), true);
+                            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+                            InputHandler inHandler = new InputHandler(client);
+                            Thread t = new Thread(inHandler);
+                            t.start();
+                            t.stop();
+
+                        }catch (IOException e){
+
+                        }
+
+                        //TODO:
+                    }
+                    else if(message.equals("quit")){
                         inReader.close();
                         shutdown();
                     }else{
-                        out.println(message);
+                        out.println(message);// TODO: this should be removed as we don't want this to output the message
+                        //I dont think this commented code below is important, but could be useful.
+                        //this.client.getOutputStream().write(message.getBytes());//set message to byte array and send to server
                     }
 
                 }
