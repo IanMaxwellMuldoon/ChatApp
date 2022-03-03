716f5405a32ca727641153ed92d5e5d37fe6696f
Ian Muldoon <ian.muldoon.465@my.csun.edu>
2022-03-03 11:49:50 -0800

    Deleted Server.java

--- src/main/java/ChatApp.java
+++ src/main/java/ChatApp.java
@@ -1,12 +1,11 @@
-import javax.print.attribute.standard.Destination;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 import java.util.*;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
 
 public class ChatApp {
     private int myport;
@@ -183,7 +182,7 @@ public class ChatApp {
                         System.out.println("Exiting");
                         exit();
                         System.exit(0);
-                    } 
+                    }
                 } else {
                     System.out.println("Invalid command \n");
                 }
